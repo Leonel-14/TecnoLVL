ed057fdfc03d983e055a8d1482c2fdda5d4ac5a9
Leonel-14 <leonelrodriguez142001@gmail.com>
2024-11-05 18:57:35 -0300

    update

--- apps/producto/urls.py
+++ apps/producto/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from apps.producto.views import ProductoView
 urlpatterns = [
-    path('productos/',ProductoView.as_view()),
+    path('productos/',ProductoView.as_view(),name='productos'),
 ]
