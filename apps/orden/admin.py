92be0fe1da9708d5431c6155726899dc8dadde43
Leonel.Rodriguez-14 <78382395+Leonel-14@users.noreply.github.com>
2024-11-18 12:28:39 -0300

    Delete apps directory

--- apps/orden/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from apps.orden.models import Orden
-# Register your models here.
-@admin.register(Orden)
-class OrdenAdmin(admin.ModelAdmin):
-    list_display = ('id_venta','id_cliente','direccion_establecida','fecha_entrega')
--- apps/producto/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path
-from apps.producto.views import ProductoView
-urlpatterns = [
-    path('productos/',ProductoView.as_view(),name='productos'),
-]
